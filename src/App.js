768c4814eb5bd814a8441163984e3e24bd71ccdd
Tiger Sun <suntiger.stdt@gmail.com>
2021-01-11 10:40:19 -0800

    Added Delete Functionality

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import Table from './Table'
 
 class App extends Component {
-  render() {
-    const characters = [
+  state = {
+    characters: [
       {
         name: 'Charlie',
         job: 'Janitor',
@@ -21,13 +21,52 @@ class App extends Component {
         job: 'Bartender',
       },
     ]
+  }
+  removeCharacter = index => {
+    const { characters } = this.state
 
-    return (
-      <div className="container">
-        <Table characterData={characters} />
-      </div>
-    )
+    this.setState({
+    characters: characters.filter((character, i) => {
+      return i !== index
+    }),
+  })
+}
+  render() {
+	  const { characters } = this.state;
+	
+	  return (
+		  <div className="container">
+			  <Table characterData={characters} removeCharacter={this.removeCharacter} />
+		  </div>
+	  );
   }
 }
+//   render() {
+//     const characters = [
+//       {
+//         name: 'Charlie',
+//         job: 'Janitor',
+//       },
+//       {
+//         name: 'Mac',
+//         job: 'Bouncer',
+//       },
+//       {
+//         name: 'Dee',
+//         job: 'Aspring actress',
+//       },
+//       {
+//         name: 'Dennis',
+//         job: 'Bartender',
+//       },
+//     ]
+
+//     return (
+//       <div className="container">
+//         <Table characterData={characters} />
+//       </div>
+//     )
+//   }
+// }
 
 export default App
